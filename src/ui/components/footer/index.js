b77bb65d7522340abd25e29f37b401bb26c6facd
phathuynhtranson99 <phat.huynhtranson99@gmail.com>
2021-09-09 19:50:32 -0600

    Fix link in footer

--- src/ui/components/footer/index.js
+++ src/ui/components/footer/index.js
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 import { breakpoints } from "../../styling/theme";
 import { Icon } from "../typography";
+import { Link } from "react-router-dom";
 
 
 const Wrapper = styled.footer`
     display: flex;
     flex-flow: row nowrap;
+    align-items: start;
     padding: 2rem; 
     border-top: 2px solid #eee;
 
@@ -47,6 +49,11 @@ const LinkSection = styled.div`
         color: ${props => props.theme.black}
     }
 
+    & a {
+        text-decoration: none;
+        color: ${props => props.theme.black};
+    }
+
     ${breakpoints.sm} {
         flex: 0 0 100%;
         text-align: center;
@@ -70,8 +77,11 @@ export const Footer = () => {
                     <h3>Website</h3>
                     <nav>
                         <ul>
-                            <li>Read all news</li>
-                            <li>Contact</li>
+                            <li>
+                                <Link to="/">
+                                    Read all news
+                                </Link>
+                            </li>
                         </ul>
                     </nav>
                 </div>
@@ -82,8 +92,16 @@ export const Footer = () => {
                     <h3>Account</h3>
                     <nav>
                         <ul>
-                            <li>Login</li>
-                            <li>Sign up</li>
+                            <li>
+                                <Link to="/login">
+                                    Login
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/signup">
+                                    Sign up
+                                </Link>
+                            </li>
                         </ul>
                     </nav>
                 </div>
